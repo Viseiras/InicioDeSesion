bb750d5140466b9542d478a65988e09bc8f8a8eb
Viseiras <javi.vice@gmail.com>
2022-12-01 22:13:29 +0100

    Anyir

--- app/src/main/java/com/example/iniciodesesion/MainActivity.java
+++ app/src/main/java/com/example/iniciodesesion/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.iniciodesesion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button= findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+                    Intent intent = new Intent(this, ItemFragment.class);
+                    startActivity(intent);
+                }
+        );
     }
 }
